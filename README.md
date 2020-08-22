e6fa12cb9b951049fd237baad4e3be39ad0e1dd6
Anthony Odero <anthonyoyoo@gmail.com>
2020-08-21 20:32:10 -0700

    added user stories

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # anthonyOdero-bookmarks-app
+
+User Stories completed:
+
--- src/index.js
+++ src/index.js
@@ -3,3 +3,22 @@
 import $ from 'jquery';
 import './index.css';
 
+
+
+// ************************************************************************
+// user stories 
+
+// User can add bookmark containing title, url link, descrition, rating (1-5)
+// User can see list of bookmarks on landing page 
+// User can click on bookmark to display detailed view 
+// User can remove bookmark from list
+// User receives feedback when submit fails
+// User can filter bookmarks by rating
+// Extension feature: user can edit bookmark rating and descrition
+
+
+// ************************************************************************
+
+// function stubs
+
+
